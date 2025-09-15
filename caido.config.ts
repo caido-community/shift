08939ed523316de01eed4c634b9d728485cd55e9
bebiksior <bebiks.priv@gmail.com>
2025-09-15 22:39:09 +0200

    bump version

--- caido.config.ts
+++ caido.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   id,
   name: "Shift",
   description: "Delegate your work to Shift",
-  version: "1.0.3",
+  version: "1.0.4",
   author: {
     name: "Caido Labs Inc.",
     email: "dev@caido.io",
